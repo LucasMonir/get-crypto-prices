43b57203c092e6ec6b21e2416fcfe54b4498954d
Lucas <55569386+LucasMonir@users.noreply.github.com>
2021-08-03 13:43:43 -0300

    Fixed fortmatting

--- get-crypto-prices.py
+++ get-crypto-prices.py
@@ -1,4 +1,3 @@
-from logging import error
 from time import time
 from win10toast import ToastNotifier
 import os
@@ -15,14 +14,14 @@ try:
         coins = {}
         prices = ''
 
-        coins['BTC'] = client.get_symbol_ticker(symbol="BTCUSDT")['price']
-        coins['EOS'] = client.get_symbol_ticker(symbol="EOSUSDT")['price']
-        coins['XTZ'] = client.get_symbol_ticker(symbol="XTZUSDT")['price']
-        coins['ETH'] = client.get_symbol_ticker(symbol="ETHUSDT")['price']
-        coins['ADA'] = client.get_symbol_ticker(symbol="ADAUSDT")['price']
+        coins['BTC'] = client.get_symbol_ticker(symbol="BTCUSDT")['price'][:8]
+        coins['EOS'] = client.get_symbol_ticker(symbol="EOSUSDT")['price'][:8]
+        coins['XTZ'] = client.get_symbol_ticker(symbol="XTZUSDT")['price'][:8]
+        coins['ETH'] = client.get_symbol_ticker(symbol="ETHUSDT")['price'][:8]
+        coins['ADA'] = client.get_symbol_ticker(symbol="ADAUSDT")['price'][:8]
         
         for coin in coins:
-            prices += coin + '{0:.3}'.format(coins[coin]) 
+            prices += f'{coin}: {coins[coin]}' 
             prices += '\n'
 
         toast = ToastNotifier() 

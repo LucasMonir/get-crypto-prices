08ae01dc174347e8e36816a3a06e35ba483c85a0
Lucas <55569386+LucasMonir@users.noreply.github.com>
2021-08-04 08:27:03 -0300

    Now it reads the crypto initials from a JSON
    Code refactored, replaced while with a for, instead of inserting the coins the hard way a json file was added.

--- get-crypto-prices.py
+++ get-crypto-prices.py
@@ -1,33 +1,30 @@
-from time import time
+from time import sleep
 from win10toast import ToastNotifier
-import os
+from os import environ
 from binance import Client
-import time
-
+import json
 try:
-    api_key = os.environ.get('binance_api')
-    api_secret = os.environ.get('binance_secret')
+    api_key = environ.get('binance_api')
+    api_secret = environ.get('binance_secret')
     client = Client(api_key, api_secret)
     client.API_URL = 'https://api.binance.com/api'
 
-    while range(10):
-        coins = {}
+    for range in range(100):
         prices = ''
-
-        coins['BTC'] = client.get_symbol_ticker(symbol="BTCUSDT")['price'][:8]
-        coins['EOS'] = client.get_symbol_ticker(symbol="EOSUSDT")['price'][:8]
-        coins['XTZ'] = client.get_symbol_ticker(symbol="XTZUSDT")['price'][:8]
-        coins['ETH'] = client.get_symbol_ticker(symbol="ETHUSDT")['price'][:8]
-        coins['ADA'] = client.get_symbol_ticker(symbol="ADAUSDT")['price'][:8]
         
-        for coin in coins:
-            prices += f'{coin}: {coins[coin]}' 
-            prices += '\n'
+        with open("coins.json") as coins:
+            json = json.load(coins)
+            coins.close()        
+    
+        for coin in json:
+            prices +=  client.get_symbol_ticker(symbol=f'{json[coin]}USDT')['price'][:8]
+            prices += "\n"
 
         toast = ToastNotifier() 
         toast.show_toast("Your prices:", prices)
         
-        time.sleep(5 * 60)
+        del coins, prices
+        sleep(5 * 60)
 except Exception as ex:
     raise ex
 

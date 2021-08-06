008bcbbb030355d184ec1509d05eed73710b1d00
Lucas <55569386+LucasMonir@users.noreply.github.com>
2021-08-06 07:47:22 -0300

    Delete ubuntu_testing.py

--- ubuntu_testing.py
+++
@@ -1,8 +0,0 @@
-import notify2 
-notify2.init('app name')
-n = notify2.Notification('title', 'message  \n hentai \n hentai \n hentai \n hentai \n hentai \n hentai \n  \n hentai \n hentai \n hentai \n hentai \n hentai \n hentai \n  \n hentai \n hentai \n hentai \n hentai \n hentai \n hentai \n ')
-n.show()
-
-import notify2 else win10toast 
-
-print(os.name)
